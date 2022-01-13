cb0093cd33df75bf9f061539e7dfac0415eea17e
Samuel Bancal <Samuel.Bancal@epfl.ch>
2022-01-13 17:23:47 +0100

    ➕ (frontend): vue2-leaflet

---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    This is the map
+    <l-map style="height: 300px" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <!-- <l-marker :lat-lng="markerLatLng"></l-marker> -->
+    </l-map>
+  </v-container>
+</template>
+
+<script>
+// import L from 'leaflet';
+import { LMap, LTileLayer } from "vue2-leaflet"; // LMarker
+import { Icon } from "leaflet";
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
+
+export default {
+  name: "Map",
+  components: {
+    LMap,
+    LTileLayer,
+    // LMarker,
+  },
+  data() {
+    return {
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 9,
+      center: [52.5, 13.3],
+      // markerLatLng: [51.504, -0.159],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
   <v-container>
     <h1>Hello</h1>
+    <Map />
   </v-container>
 </template>
 
 <script>
+// @ is an alias to /src
+import Map from "@/components/Map.vue";
 export default {
   name: "Home",
+  components: {
+    Map,
+  },
 };
 </script>
