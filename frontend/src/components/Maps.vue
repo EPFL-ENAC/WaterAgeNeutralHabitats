1d1b4c79a8fe9c21857f9fe0187be622c27b5579
Samuel Bancal <Samuel.Bancal@epfl.ch>
2022-01-20 19:42:01 +0100

    ✨ (Leaflet): +from vanilla +2 maps sync

---
+++ frontend/src/components/Maps.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title> Linked maps </v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col cols="5">
+            <div id="map1" />
+          </v-col>
+
+          <v-col cols="5">
+            <div id="map2" />
+          </v-col>
+
+          <v-col cols="2">
+            <v-card>
+              <v-card-title> Map controls </v-card-title>
+              <v-card-text> to come here... </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import L from "leaflet";
+require("leaflet.sync");
+
+export default {
+  name: "MapsVanilla",
+  data() {
+    return {
+      tilesUrl: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 8,
+      center: [52.5, 13.3],
+    };
+  },
+  mounted() {
+    const maps = new Array(2).fill().map((_val, index) =>
+      L.map(`map${index + 1}`, {
+        layers: [
+          L.tileLayer(this.tilesUrl, {
+            attribution: this.attribution,
+          }),
+        ],
+        center: this.center,
+        zoom: this.zoom,
+      })
+    );
+    maps[0].sync(maps[1]);
+    maps[1].sync(maps[0]);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#map1,
+#map2 {
+  height: 300px;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <v-container>
     <h1>Hello</h1>
-    <Map />
+    <Maps />
   </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import Map from "@/components/Map.vue";
+import Maps from "@/components/Maps.vue";
 export default {
   name: "Home",
   components: {
-    Map,
+    Maps,
   },
 };
 </script>
