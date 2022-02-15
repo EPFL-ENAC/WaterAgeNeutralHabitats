5de6161fbe01c34acd1d75832c9745e0a174756a
Samuel Bancal <Samuel.Bancal@epfl.ch>
2022-02-15 14:30:53 +0100

    ✨ (App naming): Water Age Neutral Habitats

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Water Age Neutral Habitats";
+      return args;
+    });
+  },
 };
