ca24c123a0073d3ffc5885bf407be5977868b7ce
Samuel Bancal <Samuel.Bancal@epfl.ch>
2022-03-15 14:49:11 +0100

    💄 (Home): title
    WIP #28

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,13 +3,9 @@
     <v-row>
       <v-col cols="4">
         <v-card flat>
-          <v-card-title> Berlin Water Age Neutral Habitats </v-card-title>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
+          <v-card-title class="text-h4" :style="{ wordBreak: 'break-word' }">
+            Berlin Water Age Neutral Habitats
+          </v-card-title>
           <Controls />
         </v-card>
       </v-col>
