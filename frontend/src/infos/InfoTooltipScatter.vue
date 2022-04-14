da123a54e884be0a11bb781c427ba5aec40bb28d
Samuel Bancal <Samuel.Bancal@epfl.ch>
2022-04-14 17:44:12 +0200

    💬 (InfoTooltips): Scatterplot
    WIP #9

--- frontend/src/infos/InfoTooltipScatter.vue
+++ frontend/src/infos/InfoTooltipScatter.vue
@@ -1,9 +1,38 @@
 <template>
   <InfoTooltip category="info">
-    Fraction of surface runoff [or evapotranspiration or soil leakage, if they
-    will be included] production against the fraction of permeable surface area.
-    Each marker indicates an individual design strategy for an urban fabric
-    type.
+    <v-card>
+      <v-card-text>
+        <p>
+          Fraction of surface runoff production against the fraction of
+          permeable surface area. Each marker indicates an individual design
+          strategy for an urban fabric type.
+        </p>
+
+        <div class="text-h6">Key Functionalities:</div>
+        <v-list>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-circle-small</v-icon>
+            </v-list-item-icon>
+            Click on any urban fabric type to select/de-select the corresponding
+            points
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-circle-small</v-icon>
+            </v-list-item-icon>
+            Zoom in the chart with the zoom buttons in the upper right corner
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-circle-small</v-icon>
+            </v-list-item-icon>
+            Download the chart as an image with the download button in the upper
+            right corner
+          </v-list-item>
+        </v-list>
+      </v-card-text>
+    </v-card>
   </InfoTooltip>
 </template>
 
