a61f3ba4ee8869ca4474335597fbc346816cbb14
Samuel Bancal <Samuel.Bancal@epfl.ch>
2022-05-10 10:24:21 +0200

    💄 (TimeseriesLegend): more compact
    WIP #53

--- frontend/src/components/TimeseriesLegend.vue
+++ frontend/src/components/TimeseriesLegend.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="d-flex flex-row align-end">
     <v-card flat>
-      <v-card-title class="pt-0 pb-0">
+      <v-card-title>
         Water fluxes in time
         <InfoTooltipTimeseries />
       </v-card-title>
 
       <v-card-text>
         <div>
-          <div v-for="(serie, index) in TIMESERIES_LINES_ATTRS" :key="index">
-            <v-card flat>
-              <v-card-title>
-                {{ serie.name }}
-              </v-card-title>
-              <v-card-text>
-                <div
-                  class="d-flex flex-row"
-                  v-for="entry in serie.entries"
-                  :key="entry.label"
-                >
-                  <div
-                    class="color-square ma-1"
-                    :style="{ backgroundColor: entry.color }"
-                  />
-                  {{ entry.label }}
-                </div>
-              </v-card-text>
-            </v-card>
+          <div
+            class="ma-3"
+            v-for="(serie, index) in TIMESERIES_LINES_ATTRS"
+            :key="index"
+          >
+            <div class="v-label mb-1">
+              {{ serie.name }}
+            </div>
+            <div
+              class="d-flex flex-row"
+              v-for="entry in serie.entries"
+              :key="entry.label"
+            >
+              <div
+                class="color-square ma-1"
+                :style="{ backgroundColor: entry.color }"
+              />
+              {{ entry.label }}
+            </div>
           </div>
         </div>
       </v-card-text>
